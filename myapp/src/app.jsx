4d5378d8cfee57df5245079a68b1d4ed1c8f96f7
Robert Czajka <robchike9290@gmail.com>
2022-03-23 20:43:23 -0500

    Change routing method back to BrowserRouter, remove '#' signs from routes.

--- myapp/src/app.jsx
+++ myapp/src/app.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import Select from 'react-select';
 import { hot } from 'react-hot-loader/root';
@@ -32,7 +32,7 @@ const App = () => {
 
   const handleSearchSubmit = (opt) => {
     console.log("you've selected:", opt.label);
-    window.location.href = 'http://localhost:8080/#/takequiz';
+    window.location.href = 'http://localhost:8080/takequiz';
     setSelectedQuiz(opt.label);
   };
 
